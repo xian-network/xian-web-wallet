25930a81eae34cd3754f063051ad58376a78d75d
crosschainer <eodkwokm@gmail.com>
2024-03-13 18:38:05 +0000

    nicer ux

--- js/router.js
+++ js/router.js
@@ -3,94 +3,75 @@ var app_box = document.getElementById("app-box");
 var publicKey = null;
 var unencryptedPrivateKey = null;
 
-function changePage(page, some_data=null) {
-    app_page = page;
-    switch (app_page) {
+function changePage(page, some_data = null) {
+  app_page = page;
+  const loadHtmlAndScripts = (htmlPath) => {
+      fetch(htmlPath)
+          .then(response => response.text())
+          .then(htmlContent => insertHTMLAndExecuteScripts(app_box, htmlContent))
+          .then(() => {
+              if (page === "wallet") loadWalletPage();
+              else if (page === "receive-token") loadReceiveTokenPage();
+              else if (page === "settings") loadSettingsPage();
+              else if (page === "send-advanced-transaction") loadAdvancedTransactionPage();
+              else if (page === "send-token") document.getElementById('tokenName').innerHTML = some_data;
+              else if (page === "add-to-token-list") {
+                  document.getElementById("addTokenSuccess").style.display = "none";
+                  document.getElementById("addTokenError").style.display = "none";
+              }
+          });
+  };
+
+  switch (app_page) {
       case "get-started":
-        // load html template from templates/get-started.html
-        fetch("templates/get-started.html")
-          .then((response) => response.text())
-          .then((data) => (app_box.innerHTML = data));
-        break;
+          loadHtmlAndScripts("templates/get-started.html");
+          break;
       case "create-wallet":
-        // load html template from templates/create-wallet.html
-        fetch("templates/create-wallet.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-          });
-        break;
+          loadHtmlAndScripts("templates/create-wallet.html");
+          break;
       case "import-wallet":
-        // load html template from templates/import-wallet.html
-        fetch("templates/import-wallet.html")
-          .then((response) => response.text())
-          .then((data) => (app_box.innerHTML = data));
-        break;
+          loadHtmlAndScripts("templates/import-wallet.html");
+          break;
       case "wallet":
-        // load html template from templates/wallet.html
-        fetch("templates/wallet.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            loadWalletPage();
-          });
-        break;
+          loadHtmlAndScripts("templates/wallet.html");
+          break;
       case "password-input":
-        // load html template from templates/password-input.html
-        fetch("templates/password-input.html")
-          .then((response) => response.text())
-          .then((data) => (app_box.innerHTML = data));
-        break;
+          loadHtmlAndScripts("templates/password-input.html");
+          break;
       case "send-token":
-        // load html template from templates/send-token.html
-        fetch("templates/send-token.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            document.getElementById('tokenName').innerHTML = some_data;
-          });
-        break;
+          loadHtmlAndScripts("templates/send-token.html");
+          break;
       case "receive-token":
-        // load html template from templates/receive-token.html
-        fetch("templates/receive-token.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            loadReceiveTokenPage();
-          });
-        break;
+          loadHtmlAndScripts("templates/receive-token.html");
+          break;
       case "settings":
-        // load html template from templates/settings.html
-        fetch("templates/settings.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            loadSettingsPage();
-          });
-        break;
+          loadHtmlAndScripts("templates/settings.html");
+          break;
       case "send-advanced-transaction":
-        // load html template from templates/advanced-transaction.html
-        fetch("templates/advanced-transaction.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            loadAdvancedTransactionPage();
-          });
-        break;
+          loadHtmlAndScripts("templates/advanced-transaction.html");
+          break;
       case "add-to-token-list":
-        // load html template from templates/add-to-token-list.html
-        fetch("templates/add-to-token-list.html")
-          .then((response) => response.text())
-          .then((data) => {
-            app_box.innerHTML = data;
-            document.getElementById("addTokenSuccess").style.display = "none";
-            document.getElementById("addTokenError").style.display = "none";
-          });
-        break;
+          loadHtmlAndScripts("templates/add-to-token-list.html");
+          break;
       default:
-        break;
-    }
+          break;
+  }
 }
+
+function insertHTMLAndExecuteScripts(container, htmlContent) {
+  container.innerHTML = htmlContent;
+  const scripts = container.querySelectorAll("script");
+  scripts.forEach((originalScript) => {
+      const script = document.createElement("script");
+      if (originalScript.src) {
+          script.src = originalScript.src;
+      } else {
+          script.textContent = originalScript.textContent;
+      }
+      document.head.appendChild(script).parentNode.removeChild(script);
+  });
+}
+
 
 document.addEventListener('DOMContentLoaded', (event) => {
     if (readSecureCookie('publicKey') && readSecureCookie('encryptedPrivateKey') && unencryptedPrivateKey != null) {
