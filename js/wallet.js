02015651cb72516c38be6157fc2d9b432427100c
crosschainer <eodkwokm@gmail.com>
2024-03-18 13:32:26 +0000

    fix style

--- js/wallet.js
+++ js/wallet.js
@@ -7,7 +7,7 @@ function loadWalletPage() {
     tokenList.innerHTML = "";
     tokenList.innerHTML += `<div class="title-container">
         <h2 class="token-list-title">Tokens</h2>
-        <i class="fas fa-plus-circle cogwheel-icon" onclick="changePage('add-to-token-list')"></i>
+        <i class="fas fa-plus-circle cogwheel-icon" onclick="changePage('add-to-token-list')" title="Add Token"></i>
     </div>`
 
     token_list.forEach((token) => {
@@ -26,7 +26,7 @@ function loadWalletPage() {
                 <div class="token-details">
                     <div class="token-title-container">
                         <div class="token-name"><span>`+tokenInfo["name"]+`</span> (<span class="token-symbol">`+tokenInfo["symbol"]+`</span>)</div>
-                        ` + (tokenInfo["contract"] === "currency" ? "" : `<i class="fas fa-minus-circle cogwheel-icon" onclick="removeToken('`+tokenInfo["contract"]+`')"></i>`) + `
+                        ` + (tokenInfo["contract"] === "currency" ? "" : `<i class="fas fa-minus-circle cogwheel-icon" onclick="removeToken('`+tokenInfo["contract"]+`')" title="Remove Token"></i>`) + `
                     </div>
                     <div class="token-balance" id="`+tokenInfo["contract"]+`Balance">0</div>
                 </div>    
