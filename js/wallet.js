08b05d93d37b31dca43b74fae58b5dda03ccc7f7
crosschainer <eodkwokm@gmail.com>
2024-03-25 12:52:42 +0000

    dapps are possible

--- js/wallet.js
+++ js/wallet.js
@@ -45,7 +45,7 @@ function loadWalletPage() {
         }
     });
     tokenList.innerHTML += `<a href="#" class="advanced-tx-link" onclick="changePage('send-advanced-transaction')">Create Advanced Transaction</a>` 
-    tokenList.innerHTML += `<a href="#" class="advanced-tx-link" onclick="visitDApp()">Visit a DApp</a>`
+    tokenList.innerHTML += `<a href="#" class="advanced-tx-link" onclick="visitDApp()">Visit a DApp with Wallet Connection</a>`
 }
 
 // Get current stamp rate
