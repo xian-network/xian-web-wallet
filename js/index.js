f32fdafb3a7640c00c0623387db7f04bd9f422e0
crosschainer <eodkwokm@gmail.com>
2024-04-28 06:22:44 +0000

    lock wallet

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ document.getElementById('side-change-page-ide').addEventListener('click', functi
 document.getElementById('side-change-page-wallet').addEventListener('click', function() {
     changePage('wallet');
 });
+
+document.getElementById('side-lock-wallet').addEventListener('click', function() {
+    lockWallet();
+});
+
+function lockWallet() {
+    let confirm_lock = confirm("Are you sure you want to lock the wallet?");
+    if (!confirm_lock) {
+        return;
+    }
+    // Locks the wallet
+    unencryptedPrivateKey = null;
+    locked = true;
+    changePage('password-input');
+}
