1e81732b082af862f57d28c0381f2311f25a3e7f
crosschainer <eodkwokm@gmail.com>
2024-06-01 16:48:58 +0000

    testfix

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
 let appTabId = null;
 
+// Listener for the extension icon click
 chrome.action.onClicked.addListener(function(tab) {
     const url = chrome.runtime.getURL('index.html');
 
     // Check if the tab is still open
-    if (appTabId) {
+    if (appTabId !== null) {
         chrome.tabs.get(appTabId, function(existingTab) {
             if (chrome.runtime.lastError || !existingTab) {
                 // If the tab is no longer open or an error occurred, create a new tab
@@ -18,29 +19,46 @@ chrome.action.onClicked.addListener(function(tab) {
         // If no tab ID is stored, create a new tab
         createTab();
     }
-
-    function createTab() {
-        chrome.tabs.create({url: url}, function(newTab) {
-            // Update the global variable with the new tab ID
-            appTabId = newTab.id;
-        });
-    }
 });
 
+// Function to create a new tab
+function createTab() {
+    const url = chrome.runtime.getURL('index.html');
+    chrome.tabs.create({url: url}, function(newTab) {
+        // Update the global variable with the new tab ID
+        appTabId = newTab.id;
+    });
+}
 
+// Listener for messages from the content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'dAppSendTransaction' || message.type === 'getWalletInfo') {
-        if (!appTabId && message.type === 'getWalletInfo') { // If the extension is not open, return an empty response
+        if (appTabId === null && message.type === 'getWalletInfo') { // If the extension is not open, return an empty response
             sendResponse({address: '', locked: true, chainId: ''});
             return;
         }
-        if (!appTabId && message.type === 'dAppSendTransaction') { // If the extension is not open, return an error response
+        if (appTabId === null && message.type === 'dAppSendTransaction') { // If the extension is not open, return an error response
             sendResponse({errors: ['Extension not open']});
             return;
         }
         // Forward the message to the extension window
-        chrome.runtime.sendMessage(message, sendResponse);
+        chrome.tabs.sendMessage(appTabId, message, sendResponse);
     }
     // Make sure to return true to indicate that you will send a response asynchronously
     return true;
+});
+
+// Listener for when a tab is removed
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    if (tabId === appTabId) {
+        appTabId = null;
+    }
+});
+
+// Listener for when a tab is updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tabId === appTabId && changeInfo.status === 'complete') {
+        // Update appTabId if needed, such as reloading the tab
+        appTabId = tabId;
+    }
 });
