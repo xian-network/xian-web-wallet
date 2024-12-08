5d8d49f0ca4b0ce96befe99b7d90e317dee164d8
crosschainer <eodkwokm@gmail.com>
2024-12-08 14:01:59 +0100

    fixes

--- templates/page_js/messenger.js
+++ templates/page_js/messenger.js
@@ -243,7 +243,7 @@ function switchChat(address_chat) {
         }
 
         message_content.classList.add('messenger-chat-item-content');
-        message_text.innerText = message.message;
+        message_text.innerText = sanitizeHTML(message.message);
         message_timestamp.innerText = new Date(message.timestamp).toLocaleString();
         if (message.sender === publicKey) {
             message_timestamp.innerText += ' (You)';
